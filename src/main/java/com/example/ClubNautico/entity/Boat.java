3db1f9439569792f4df3a17033bbc407d517f140
JereDAM <usr3593@salesianos-lacuesta.net>
2024-07-31 10:55:50 +0100

    Feature: Basic boat attributes done

--- src/main/java/com/example/ClubNautico/entity/Boat.java
+++ src/main/java/com/example/ClubNautico/entity/Boat.java
@@ -1,5 +1,31 @@
 package com.example.ClubNautico.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "boat")
 public class Boat {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
 
 }
