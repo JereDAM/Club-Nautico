773775639596aa71d2f007f269bd4720aa17cc32
JereDAM <usr3593@salesianos-lacuesta.net>
2024-07-31 11:05:20 +0100

    Feature: Basic patron attributes done

--- src/main/java/com/example/ClubNautico/entity/Patron.java
+++ src/main/java/com/example/ClubNautico/entity/Patron.java
@@ -1,5 +1,32 @@
 package com.example.ClubNautico.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "patron")
 public class Patron {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "dni")
+	private String dni;
 
 }
