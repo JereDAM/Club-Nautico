c77c70224df82bb274c53905b1d41891062c2ab8
JereDAM <usr3593@salesianos-lacuesta.net>
2024-07-31 12:38:12 +0100

    Feature: Exit entity done

--- src/main/java/com/example/ClubNautico/entity/Exit.java
+++ src/main/java/com/example/ClubNautico/entity/Exit.java
@@ -1,5 +1,32 @@
 package com.example.ClubNautico.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "exit")
 public class Exit {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "created")
+	private LocalDateTime created;
 }
