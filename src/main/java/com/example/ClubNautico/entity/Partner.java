8d4c3b3d46e69b8b6103a9b3ce338442883909d1
JereDAM <usr3593@salesianos-lacuesta.net>
2024-07-31 12:25:17 +0100

    Feature: All basics attributes added to entities

--- src/main/java/com/example/ClubNautico/entity/Partner.java
+++ src/main/java/com/example/ClubNautico/entity/Partner.java
@@ -1,5 +1,33 @@
 package com.example.ClubNautico.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import jakarta.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "partner")
 public class Partner {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "dni")
+	private String dni;
 }
